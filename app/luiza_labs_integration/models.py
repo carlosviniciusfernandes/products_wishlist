cfe64ba3bb19dc5186d376203ccd05bbd6f47c10
Carlos Vinicius Fernandes <cfernandes@daitan.com>
2022-07-18 21:33:47 -0300

    Created Django App luiza_labs_integration

---
+++ app/luiza_labs_integration/models.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+from typing import Optional
+from uuid import uuid4
+
+
+@dataclass
+class Product:
+    id: uuid4
+    price: float
+    image: str
+    brand: str
+    title: str
+    reviewScore: Optional[float]
---
+++ app/luiza_labs_integration/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+PRODUCT_API_BASE_URL='http://challenge-api.luizalabs.com/api/product/'
+
+
+def list_products(page):
+    """PRODUCT_API_BASE_URL?page=<PAGINA>"""
+    pass
+
+
+def retrieve_product_details(id):
+    """PRODUCT_API_BASE_URL<ID>/"""
+    pass
