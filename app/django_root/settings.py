866e2d3dc37c8b4f7c6ac1e0c671cd62cea762f3
Carlos Vinicius Fernandes <cfernandes@daitan.com>
2022-07-24 09:10:57 -0300

    Added Djando Rest Auth (dj_rest_auth) for user authentication

--- app/django_root/settings.py
+++ app/django_root/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
     'wishlist',
     'luiza_labs'
 ]
--- app/django_root/urls.py
+++ app/django_root/urls.py
@@ -18,5 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('auth/', include('dj_rest_auth.urls')),
+    path('wishlist/', include('wishlist.urls')),
 ]
